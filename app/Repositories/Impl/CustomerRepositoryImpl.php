954c0699f6de2e119d3b54bc6bc95bdd3d4629a8
Le-Quynh-29 <bachcongtu2904@gmail.com>
2020-12-24 21:22:32 +0700

    restful

---
+++ app/Repositories/Impl/CustomerRepositoryImpl.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Repositories\Impl;
+
+use App\Models\Customer;
+use App\Repositories\CustomerRepository;
+use App\Repositories\Eloquent\EloquentRepository;
+
+class CustomerRepositoryImpl extends EloquentRepository  implements CustomerRepository
+{
+    /**
+     * get Model
+     * @return string
+     */
+    public function getModel()
+    {
+        $model = Customer::class;
+        return $model;
+    }
+}
